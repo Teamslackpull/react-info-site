6dad479f8a28b7af9a83b44e355a7123c49d4761
Nabil-Teamslackpull <nabil.99@live.nl>
2024-07-02 19:58:54 +0200

    upload files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
+
+const Header = () => {
+    return (
+            <header>
+                <nav>
+                    <div className="LogoContainer">
+                        <img src={logo} className="App-logo" alt="logo" width="30%"/>
+                        <h2>React Facts</h2>
+                    </div>
+                    <ul>
+                        <li>React Course - Project 1</li>
+                    </ul>
+                </nav>
+            </header>
+    )
+
+}
+
+const ReactList = () => {
+    return (
+        <ul className="ReactList">
+            <li>Its a new skill</li>
+            <li>You can build amazing things</li>
+            <li>High demand</li>
+            <li>And its fast</li>
+            <li>You can also build Native apps </li>
+        </ul>
+    )
+
+}
+
+const Footer = () => {
+    return(
+
+        <small>
+            <footer> @2024 DevDreams </footer>
+        </small>
+    )
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="Card">
+            <Header/>
+            <div style={{backgroundImage:`url(${logo})`}} className="Content">
+                <div className='Bottom'>
+                    <h1 className="Title">Fun Facts about react</h1>
+                    <ReactList/>
+                </div>
+            </div>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App;
+
